dc8df2bd9d231e4bcfd0acef9e9450fcfc8f0b89
Lidessen <ztytotoro@outlook.com>
2024-12-01 20:12:51 +0800

    update build

--- esbuild.config.ts
+++ esbuild.config.ts
@@ -1,13 +1,13 @@
-import fs from "fs";
 import * as esbuild from "esbuild";
 
-const code = fs.readFileSync("./cli.ts", "utf-8");
-
-const result = await esbuild.transform(code, {
+await esbuild.build({
+  entryPoints: ["./cli.ts"],
+  outfile: "./dist/cli.js",
+  bundle: true,
   platform: "node",
-  loader: "ts",
   format: "esm",
-  banner: "#!/usr/bin/env node\n",
+  banner: {
+    js: "#!/usr/bin/env node\n",
+  },
+  packages: "external",
 });
-
-fs.writeFileSync("./dist/cli.js", result.code);
